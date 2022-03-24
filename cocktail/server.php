5ca391ed6aba33e3ca7a9736f056be7a0cb8c363
Sean Archer Turqueza <50150118+stt9@users.noreply.github.com>
2022-03-23 20:26:15 -0400

    Update server.php

--- cocktail/server.php
+++ cocktail/server.php
@@ -17,8 +17,7 @@ if (isset($_POST['reg_user'])) {
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
+ 
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
@@ -26,7 +25,7 @@ if (isset($_POST['reg_user'])) {
 	array_push($errors, "The two passwords do not match");
   }
 
-  // first check the database to make sure 
+  
   // a user does not already exist with the same username and/or email
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
@@ -42,7 +41,7 @@ if (isset($_POST['reg_user'])) {
     }
   }
 
-  // Finally, register user if there are no errors in the form
+ //register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//pasword encrypt
 
